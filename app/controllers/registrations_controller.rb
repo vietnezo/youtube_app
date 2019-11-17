58368f3e4f26cb2ff6bb013867521fd899fc4527
Tran Trong Viet <t.t.viet@azoom.jp>
2019-11-17 11:55:58 +0900

    add login/register action

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,28 @@
+class RegistrationsController < Devise::RegistrationsController
+  prepend_before_action :allow_params_authentication!, only: :create
+
+  def auth_options
+    { scope: resource_name, recall: "#{controller_path}#new" }
+  end
+
+  def create
+    if User.exists?(email: sign_up_params["email"])
+      self.resource = warden.authenticate!(auth_options)
+      set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      super
+    end
+  end
+
+  protected
+  def update_resource(resource, params)
+    if params[:password].present?
+      resource.update params
+    else
+      resource.update_without_password(params)
+    end
+  end
+end
